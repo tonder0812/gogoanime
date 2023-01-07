53c091a53d4b1130d85a886ab7d5bdfac82aa80d
tonder0812 <tonderbals@gmail.com>
2023-01-07 11:12:07 +0000

    Added pip install to install.bat

--- install.bat
+++ install.bat
@@ -1,8 +1,11 @@
 @echo off
+pushd "%~dp0"
+pip install -r requirements.txt
 mkdir config
 pushd config
 type nul > new.txt
 type nul > quit.txt
 type nul > watching.txt
 echo {} > config.json
+popd
 popd
