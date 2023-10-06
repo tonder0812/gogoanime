e1680ec958c2d3ea96f36fbddbe2afda0891ebb9
tonder0812 <tonderbals@gmail.com>
2023-10-06 20:25:31 +0100

    improve selection of highest quality

--- parsers/video_link.py
+++ parsers/video_link.py
@@ -8,12 +8,20 @@ class VideoLinkParser(Parser):
     def __init__(self, contents: str):
         super().__init__(contents)
         self.downloadLink = None
+        self.best_resolution = 0
 
     def handle_data(self, data: str):
         data = data.strip()
         if data == "":
             return
-        if (self.curent_tag.tag == "a"
-                and self.curent_tag.parent.attrs.get("class") == "cf-download"
-                and data in ("1280x720", "1920x1080", "2580x1080", "3840x2160")):
-            self.downloadLink = self.curent_tag.attrs.get("href")
+        if (self.curent_tag.tag == "a" and
+                self.curent_tag.parent.attrs.get("class") == "cf-download"):
+
+            dim = data.split("x")
+            if len(dim) != 2:
+                return
+
+            resolution = int(dim[0])*int(dim[1])
+            if resolution > self.best_resolution and int(dim[1]) >= 720:
+                self.downloadLink = self.curent_tag.attrs.get("href")
+                self.best_resolution = resolution
